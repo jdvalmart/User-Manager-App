f1e26a571dd9fedcbdc3679fea31b7cf1afe3b5b
jdvalmart <juanvalencia9411@outlook.com>
2025-09-29 22:25:39 -0500

    este es un proyecto llamado user-manager sobre los conceptos de react con typeScrtipt, se inicializa el proyecto, se instala tailwind, se crea la estructura de las carpetas y se crean los primeros componentes sin logica adentro

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hola Mundo</h1>
+      <p className="">Esto es una prueba de react</p>
+      
+    </>
+  )
+}
+
+export default App
